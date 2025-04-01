89fb068096e9fefeea9887a96ea3184346890f76
Avan Singh <avansingh085@gmail.com>
2025-04-02 02:14:27 +0530

    update

--- project/services/weatherApi.js
+++ project/services/weatherApi.js
@@ -1,8 +1,8 @@
 export async function fetchWeatherData(cities) {
-    // const apiKey = process.env.NEXT_PUBLIC_OPENWEATHERMAP_API_KEY;
+    const apiKey = process.env.WHEATHER_API_KEY;
     const promises = cities.map((city) =>
       fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'796339042a71f863e30fe1d307f99b60'}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
       ).then((res) => res.json())
     );
     // console.log(Promise.all(promises))
